12090801dff89ef27b730f2fd9593d798eb88537
yinet Jaquez <yinet.Jaquez@hotmail.com>
2017-06-09 22:50:49 -0400

    clases

---
+++ DetalleConKonckout/Models/Cotizaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DetalleConKonckout.Models
+{
+    public class Cotizaciones
+    {
+        [Key]
+        public int CotizacionId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Cliente { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Fecha { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Monto { get; set; }
+
+    }
+}
---
+++ DetalleConKonckout/Models/CotizacionesDetalle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DetalleConKonckout.Models
+{
+    public class CotizacionesDetalle
+    {
+        [Key]
+        public int CotizacionDetalleId { get; set; }
+
+        public int CotizacionId { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public string Producto { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public double SubTotal { get; set; }
+    }
+}
