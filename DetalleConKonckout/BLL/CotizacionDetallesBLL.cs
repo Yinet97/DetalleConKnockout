df83d6428b6da3b2305896ce7a55aa87d22cbcfa
yinet Jaquez <yinet.Jaquez@hotmail.com>
2017-06-09 23:06:35 -0400

    BLLs

---
+++ DetalleConKonckout/BLL/CotizacionDetallesBLL.cs
@@ -0,0 +1,114 @@
+﻿using DetalleConKonckout.DAL;
+using DetalleConKonckout.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DetalleConKonckout.BLL
+{
+    public class CotizacionDetallesBLL
+    {
+        public static bool Guardar(CotizacionesDetalles detalle)
+        {
+            bool resultado = false;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    conexion.CotDetalle.Add(detalle);
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static CotizacionesDetalles Buscar(int? detalleCotizacionId)
+        {
+            CotizacionesDetalles detalle = null;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    detalle = conexion.CotDetalle.Find(detalleCotizacionId);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return detalle;
+        }
+        public static bool Modificar(CotizacionesDetalles detalle)
+        {
+            bool resultado = false;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    conexion.Entry(detalle).State = EntityState.Modified;
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static bool Eliminar(CotizacionesDetalles detalle)
+        {
+            bool resultado = false;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    conexion.Entry(detalle).State = EntityState.Deleted;
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static List<CotizacionesDetalles> Listar()
+        {
+            List<CotizacionesDetalles> listado = null;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    listado = conexion.CotDetalle.ToList();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return listado;
+        }
+        public static bool Save(List<CotizacionesDetalles> detalles)
+        {
+            bool resultado = false;
+            foreach (CotizacionesDetalles detail in detalles)
+            {
+                resultado = Guardar(detail);
+            }
+            return resultado;
+        }
+    }
+}
---
+++ DetalleConKonckout/BLL/CotizacionesBLL.cs
@@ -0,0 +1,105 @@
+﻿using DetalleConKonckout.DAL;
+using DetalleConKonckout.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DetalleConKonckout.BLL
+{
+    public class CotizacionesBLL
+    {
+        public static bool Guardar(Cotizaciones cotizacion)
+        {
+            bool resultado = false;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    conexion.Cotizacion.Add(cotizacion);
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static Cotizaciones Buscar(int? cotizacionId)
+        {
+            Cotizaciones cotizacion = null;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    cotizacion = conexion.Cotizacion.Find(cotizacionId);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return cotizacion;
+        }
+        public static bool Modificar(Cotizaciones cotizacion)
+        {
+            bool resultado = false;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    conexion.Entry(cotizacion).State = EntityState.Modified;
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static bool Eliminar(Cotizaciones cotizacion)
+        {
+            bool resultado = false;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    conexion.Entry(cotizacion).State = EntityState.Deleted;
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static List<Cotizaciones> Listar()
+        {
+            List<Cotizaciones> listado = null;
+            using (var conexion = new CotizacionDb())
+            {
+                try
+                {
+                    listado = conexion.Cotizacion.ToList();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return listado;
+        }
+    }
+}
